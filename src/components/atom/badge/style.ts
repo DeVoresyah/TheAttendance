8f7ea161da9af0aec7032663b46d86ce0986713c
DeVoresyah ArEst <de.voresyah@gmail.com>
2021-11-02 23:21:17 +0700

    fix: small style badge

--- src/components/atom/badge/style.ts
+++ src/components/atom/badge/style.ts
@@ -5,7 +5,7 @@ export default connect({
   small: 'py-px px-1',
   medium: 'py-1 px-2',
   large: 'py-2 px-3',
-  labelSmall: 'text-xs font-regular text-white uppercase',
+  labelSmall: 'text-xxs font-medium text-white uppercase',
   labelMedium: 'text-sm font-medium text-white uppercase',
   labelLarge: 'text-base font-bold text-white uppercase',
 });
